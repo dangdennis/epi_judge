f595e6ec2f4a324298ef90c8e47387b443f9d129
Dennis Dang <dangggdennis@gmail.com>
2022-09-23 18:40:43 -0400

    solve generate_primes - sieve technique

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -5,8 +5,17 @@ from test_framework import generic_test
 
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    primes = []
+    maybe_primes = [False, False] + [True] * (n - 1)
+    
+    for i in range(2, len(maybe_primes)):
+        if maybe_primes[i]:
+            primes.append(i)
+            
+            for j in range(i, len(maybe_primes), i):
+                maybe_primes[j] = False
+            
+    return primes
 
 
 if __name__ == '__main__':
