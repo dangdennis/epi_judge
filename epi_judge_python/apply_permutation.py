e87dfa08a99cf1c4bda0fd8f090946efb3ba99be
Dennis Dang <dangggdennis@gmail.com>
2022-09-24 19:58:35 -0400

    feat: apply_permutation

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -1,12 +1,27 @@
+from time import perf_counter_ns
 from typing import List
 
 from test_framework import generic_test
 
 
 def apply_permutation(perm: List[int], A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+    # O(n) space and time
+    # permutated = [0] * len(A)
+    # for i in range(0, len(A)):
+    #     pos = perm[i]
+    #     permutated[pos] = A[i]
 
+    # for i in range(0, len(A)):
+    #     A[i] = permutated[i]
+    
+    # O(n) time and O(1) space
+    for i in range(len(A)):
+        next = i
+        while perm[next] >= 0:
+            A[i], A[perm[next]] = A[perm[next]], A[i]
+            temp = perm[next]
+            perm[next] = -1
+            next = temp
 
 def apply_permutation_wrapper(perm, A):
     apply_permutation(perm, A)
