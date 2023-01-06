37eaa47ff5c7685ebc1d21dda31a1f839ce43e8b
Dennis Dang <dangggdennis@gmail.com>
2023-01-06 00:52:39 -0500

    test pyright

--- epi_judge_python_solutions/advance_by_offsets.py
+++ epi_judge_python_solutions/advance_by_offsets.py
@@ -4,7 +4,6 @@ from test_framework import generic_test
 
 
 def can_reach_end(A: List[int]) -> bool:
-
     furthest_reach_so_far, last_index = 0, len(A) - 1
     i = 0
     while i <= furthest_reach_so_far and furthest_reach_so_far < last_index:
@@ -13,8 +12,9 @@ def can_reach_end(A: List[int]) -> bool:
     return furthest_reach_so_far >= last_index
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     exit(
-        generic_test.generic_test_main('advance_by_offsets.py',
-                                       'advance_by_offsets.tsv',
-                                       can_reach_end))
+        generic_test.generic_test_main(
+            "advance_by_offsets.py", "advance_by_offsets.tsv", can_reach_end
+        )
+    )
